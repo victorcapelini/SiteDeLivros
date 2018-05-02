e0fa32ab8e0ef5f2508b0ef99ac1ff79b7001e7c
Victor <victorcapelini@hotmail.com>
2018-05-02 08:12:03 -0300

    Adicionado ejs para controlar a view.

--- casadocodigo/app.js
+++ casadocodigo/app.js
@@ -1,8 +1,7 @@
-var express = require('express');
-var app = express();
+var app = require('./config/express')();
 
 app.get('/produtos',function (req,res) {
-    res.send("<html><body><h1>Listagem de produtos</h1></body></html>");    
+    res.render("produtos/lista");
 });
 
 app.listen(3000,function () {
