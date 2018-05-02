cbb703b23e176601ff5ee1c6a09437d9a8b0cb05
Victor <victorcapelini@hotmail.com>
2018-05-02 16:01:36 -0300

    Criado acesso Mysql para exibição de produtos

--- casadocodigo/app/routes/produtos.js
+++ casadocodigo/app/routes/produtos.js
@@ -1,6 +1,19 @@
-module.exports = function (app) {
-    app.get('/produtos', function (req, res) {
-        res.render("produtos/lista");
+module.exports = function(app) {
+    app.get("/produtos",function(req, res) {
+        var mysql = require('mysql');
+        var connection = mysql.createConnection({
+            host: "localhost",
+            user: "root",
+            password : "root",
+            database: "casadocodigo_nodejs"
+        });
+
+        connection.query('select * from produtos', function(err, results){
+            console.log(err);
+            res.send(results);
+        });
+
+        connection.end();
+
     });
-    return app;
 }
