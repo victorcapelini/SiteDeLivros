789045df6ade8b6fcafb653f85917998ee90a0f8
Victor <victorcapelini@hotmail.com>
2018-05-03 14:20:38 -0300

    Definido o redirect para a listagem de produtos afim de não ser possível reenviar o formulário de cadastro acidentalmente.
    Signed-off-by: Victor <victorcapelini@hotmail.com>

--- casadocodigo/app/routes/produtos.js
+++ casadocodigo/app/routes/produtos.js
@@ -1,25 +1,26 @@
 module.exports = function (app) {
-    app.get("/produtos", function (req, res) {
+     var listaProdutos=function(req,res){
         var connection = app.infra.connectionFactory();
-        var produtosBanco = new app.infra.ProdutosDao(connection);
+        var produtosDAO = new app.infra.ProdutosDao(connection);
 
-        produtosBanco.lista(function (err, results) {
+        produtosDAO.lista(function (err, results) {
             res.render('produtos/lista', { lista: results });
         });
-
         connection.end();
+    };
 
+    app.get("/produtos", listaProdutos);
+
+    app.get('/produtos/form', function (req, res) {
+        res.render('produtos/form');
     });
 
-    app.get('produtos/remove', function(){
-
-        var connection = app.infra.connectionFactory;
-        var produtosBanco = app.infra.produtosBanco;
-        var produto = produtosBanco.carrega(id,callback);
-    
-        if(produto){
-            produtosBanco.remove(produto,callback);
-        }
-    
+    app.post('/produtos', function (req, res) {
+        var produto = req.body;
+        var connection = app.infra.connectionFactory();
+        var produtosDAO = new app.infra.ProdutosDao(connection);
+        produtosDAO.salva(produto, function (erros, resultados) {
+            res.redirect('/produtos');
+        });
     });
 }
