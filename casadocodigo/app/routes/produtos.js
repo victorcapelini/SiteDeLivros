80a9ea87765f92a1399aa4a25778cd70d6dd514d
Victor <victorcapelini@hotmail.com>
2018-05-03 10:58:18 -0300

    Criado o ProdutosDao.js para acesso ao banco de dados.

--- casadocodigo/app/routes/produtos.js
+++ casadocodigo/app/routes/produtos.js
@@ -1,12 +1,25 @@
 module.exports = function (app) {
     app.get("/produtos", function (req, res) {
         var connection = app.infra.connectionFactory();
+        var produtosBanco = new app.infra.ProdutosDao(connection);
 
-        connection.query('select * from produtos', function (err, results) {
-            res.render('produtos/lista',{lista:results});
+        produtosBanco.lista(function (err, results) {
+            res.render('produtos/lista', { lista: results });
         });
 
         connection.end();
 
+    });
+
+    app.get('produtos/remove', function(){
+
+        var connection = app.infra.connectionFactory;
+        var produtosBanco = app.infra.produtosBanco;
+        var produto = produtosBanco.carrega(id,callback);
+    
+        if(produto){
+            produtosBanco.remove(produto,callback);
+        }
+    
     });
 }
